76f64dca0ea4c6bc6fbe588d7a4dd97391dad43a
Weiran Zou <zouweiran9122@gmail.com>
2024-10-11 12:00:51 +1100

    Add user profile to user places page

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -4,19 +4,22 @@ import { useParams } from "react-router-dom";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal.js";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner.js";
 import { useHttpClient } from "../../shared/hooks/http-hook.js";
+import UserItem from "../../user/components/UserItem.js";
 
 const UserPlaces = () => {
     const {isLoading, error, sendRequest, clearError} = useHttpClient();
     const [loadedPlaces, setLoadedPlaces] = useState();
+    const [loadedUser, setLoadedUser] = useState();
     const userId = useParams().userId;
-    console.log(userId);
+
     useEffect(() => {
        
         const fetchPlaces = async () => {
             try {
-                
-                const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`);
-                setLoadedPlaces(responseData.places);
+                const userResponseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/users/${userId}`);
+                const placesResponseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`);
+                setLoadedUser(userResponseData.user)
+                setLoadedPlaces(placesResponseData.places);
     
             } catch(err) {
     
@@ -37,6 +40,13 @@ const UserPlaces = () => {
                     <LoadingSpinner asOverlay/>
                 </div>
             )}
+           {!isLoading && loadedUser && 
+                <UserItem 
+                    id={loadedUser.id} 
+                    imageUrl={loadedUser.image} 
+                    name={loadedUser.name}
+                    size="large"
+                />}
            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} onDeletePlace={placeDeleteHandler} />}
         </>
         
--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -5,13 +5,13 @@ import Avatar from "../../shared/components/UIElements/Avatar"
 import Card from "../../shared/components/UIElements/Card"
 const UserItem = (props) => {
     return (
-        <div className="user-item">
+        <div className={`user-item--${props.size || 'default'} ${props.className}`}>
             {/* <Card className="user-item__content"> */}
                 <Link to={`/${props.id}/places`} >
-                    <div className="user-item__image">
+                    <div className={`user-item__image--${props.size || 'default'}`}>
                         <Avatar imageUrl={props.imageUrl} alt={props.alt} />
                     </div>
-                    <div className="user-item__info">
+                    <div className={`user-item__info--${props.size || 'default'}`}>
                         <h2>{props.name}</h2>
                         {/* <h3>{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"} </h3> */}
                     </div>
